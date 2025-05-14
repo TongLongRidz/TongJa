6b1c22f5440a2554600d9f336cf52e9d3fb580bf
TongLongRidz <chitraphanukon.w@ku.th>
2025-05-14 10:06:45 +0700

    Update Method Put (Animal)

--- demo/src/main/java/com/testing/Controller/AnimalController.java
+++ demo/src/main/java/com/testing/Controller/AnimalController.java
@@ -1,5 +1,6 @@
 package com.testing.Controller;
 
+import com.testing.Entity.AnimalEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,42 +13,46 @@ import java.util.List;
 @RequestMapping("/api/animal")
 public class AnimalController {
 
-    private final List<String> animals = new ArrayList<>(Arrays.asList("cat", "dog", "hippo"));
+    private final List<AnimalEntity> animals = new ArrayList<>(Arrays.asList(
+            new AnimalEntity(1,"Cat"),
+            new AnimalEntity(2,"Dog"),
+            new AnimalEntity(3,"Hippo")
+    ));
 
     @PostMapping("/add")
-    public ResponseEntity<String> addAnimal(@RequestBody String animal) {
+    public ResponseEntity<?> addAnimal(@RequestBody AnimalEntity animal) {
         animals.add(animal);
         return ResponseEntity.status(HttpStatus.CREATED).body(animal);
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<String>> getAllAnimals() {
+    public ResponseEntity<List<AnimalEntity>> getAllAnimals() {
         return ResponseEntity.ok(animals);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getAnimalById(@PathVariable("id") int id) {
+    public ResponseEntity<AnimalEntity> getAnimalById(@PathVariable("id") int id) {
         try {
-            return ResponseEntity.ok(animals.get(id));
-        } catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal Not Found.");
+            return ResponseEntity.ok(animals.get(id-1));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> editAnimalById(@PathVariable("id") int id, @RequestBody String animal) {
+    @PutMapping("/editName")
+    public ResponseEntity<?> editAnimalById(@RequestBody AnimalEntity animal) {
         try {
-            animals.set(id, animal);
-            return ResponseEntity.ok(animals);
-        } catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal Not Found.");
+            animals.set((int) (animal.getId()-1), animal);
+            return ResponseEntity.ok(animal);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteAnimalById(@PathVariable("id") int id) {
         try {
-            animals.remove(id);
+            animals.remove(id-1);
             return ResponseEntity.status(HttpStatus.OK).body("Animal Deleted.");
         } catch (IndexOutOfBoundsException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal Not Found.");
--- demo/src/main/java/com/testing/Entity/AnimalEntity.java
+++ demo/src/main/java/com/testing/Entity/AnimalEntity.java
@@ -1,6 +1,5 @@
 package com.testing.Entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -19,19 +18,28 @@ public class AnimalEntity {
     @Column(name="name")
     private String name;
 
-    public Long getId(){
+    public AnimalEntity() {
+
+    }
+
+    public AnimalEntity(int id, String name) {
+        this.id = (long) id;
+        this.name = name;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public String getName(){
-        return name;
-    }
-
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
